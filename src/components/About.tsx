338a60865a8a2f5b3f3928204d1a7a6cf3182e50
Paulius Vizbara <paulius1207@gmail.com>
2022-12-26 01:26:43 +0200

    Add id to about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,7 +3,7 @@ export const About = () => {
         <div className="relative overflow-hidden bg-white py-16">
             <div className="relative px-4 sm:px-6 lg:px-8">
                 <div className="mx-auto max-w-screen-xl text-lg">
-                    <h1>
+                    <h1 id="apie">
                         <span className="mt-2 block text-center text-3xl font-bold leading-8 tracking-tight text-gray-900 sm:text-4xl">
                             Apie mus
                         </span>
